9253cf6f5ec6f9c92c90493863a0aeb0d22f1229
Allen Guo <guosonglin001@gmail.com>
2023-10-23 14:27:53 +0000

    wip

--- demos/demo.sh
+++ demos/demo.sh
@@ -11,6 +11,10 @@ buck2 run --config build.type=debug examples/portable/executor_runner:executor_r
 #     --listen 5678 --wait-for-client \
 #     -m examples.portable.scripts.export_and_delegate
 
-# flatc --json -o . schema/program.fbs \
 flatc --json --defaults-json --strict-json -o . schema/program.fbs \
     -- add.pte
+
+python3 -m examples.portable.scripts.export --model_name="linear"
+buck2 run --config build.type=debug examples/portable/executor_runner:executor_runner -- --model_path linear.pte
+flatc --json --defaults-json --strict-json -o . schema/program.fbs \
+    -- linear.pte
